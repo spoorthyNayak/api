02bf5eb47a1e87bf62d7e1f016dca2f46e308d09
Uday Nayak, Spoorthy <spoorthy.uday.nayak@accenture.com>
2021-03-02 15:20:47 +0530

    commit

---
+++ src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/book")
+
+public class ApiGatewayApplication {
+	  private List<Book> books = new ArrayList<>();
+
+	    @PostMapping
+	    public Book addBook(@RequestBody Book book) {
+	        books.add(book);
+	        return book;
+	    }
+
+	    @GetMapping
+	    public List<Book> getBooks() {
+	        return books;
+	    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
